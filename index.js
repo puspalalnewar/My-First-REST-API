c84bf300909c2ad4c540c3e04a03b0e0cc14704f
Puspalal <puspalalnewar79@gmail.com>
2024-12-30 22:17:02 +0530

    Learned get and post method

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const express = require("express");
 const app = express();
 const users = require("./MOCK_DATA.json");
+const fs = require('fs');
 
 const PORT = 8000;
+
+// Middleware
+app.use(express.urlencoded({extended : false}));
 
 // Routes
 app.get('/users', (req, res) => {
     let html = `
         <ul>
-        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>
+            ${user.first_name}
+        </li>`).join("")}
         </ul>
     `
     res.send(html);
 })
 
 // REST API
+
+
 app.get('/api/users', (req, res) => {
     return res.json(users);
 })
 
 app.route("/api/users/:id")
-.get((req, res)=>{
-    const id = Number(req.params.id);
-    const user = users.find((user)=>user.id === id)
-    return res.json(user);
-})
-.patch((req, res)=>{
-    // Edit user with id
-    return res.json({status:"pending"})
-})
-.delete((req, res)=>{
-    // Delete user with id
-    return res.json({status : "Pending"})
-})
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id)
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        // Edit user with id
+        return res.json({ status: "pending" })
+    })
+    .delete((req, res) => {
+        // Delete user with id
+        return res.json({ status: "Pending" })
+    })
 
 // app.get("/api/users/:id", (req, res)=>{
 //     const id = Number(req.params.id);
@@ -40,8 +48,14 @@ app.route("/api/users/:id")
 //     return res.json(user);
 // })
 
-app.post('./api/users', (req, res)=>{
-    return res.json({ status : "Pending"});
+app.post('/api/users/details', (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
+        return res.json({ status: "Pending", user_id : users.length });
+    })
+    // console.log("Body", body);
+    
 })
 
 app.listen(PORT, () => {
